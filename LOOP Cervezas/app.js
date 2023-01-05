6bb928365a231fa17e727c373c4c89b99a58b621
pablovera866 <pablovera@outlook.com>
2023-01-05 20:30:24 -0300

    Agrego .gitignore, normalize.css y modificacion en el puerto del app.js

--- LOOP Cervezas/app.js
+++ LOOP Cervezas/app.js
@@ -6,9 +6,11 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(3000, ()=>{
-    console.log("Se prendio");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
 app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "views/index.html"))
